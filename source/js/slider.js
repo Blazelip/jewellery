be8944f6b88160e60ea5ac6e1f7040b304db3877
Blazelip <filin1994@rambler.ru>
2021-04-22 11:46:58 +0300

    Попытка сделать слайдер

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,23 +1,99 @@
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
+'use strict';
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+(() => {
+  // const slider = {
+  //   loop: true,
+  //   loopFillGroupWithBlank: true,
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     clickable: true,
+  //     type: 'bullets',
+  //     renderBullet: function (index, className) {
+  //       return '<span class="' + className + '">' + (index + 1) + '</span>';
+  //     },
+  //   },
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   },
+  //   breakpoints: {
+  //     // when window width is >= 1366px
+  //     1366: {
+  //       slidesPerView: 4,
+  //       spaceBetween: 30,
+  //       slidesPerGroup: 4,
+  //     },
+  //     // when window width is >= 768px
+  //     768: {
+  //       slidesPerView: 2,
+  //       spaceBetween: 30,
+  //       slidesPerGroup: 2,
+  //       pagination: {
+  //         type: 'bullets',
+  //       },
+  //     },
+  //     // when window width is >= 320px
+  //     320: {
+  //       slidesPerView: 2,
+  //       spaceBetween: 30,
+  //       slidesPerGroup: 2,
+  //       pagination: {
+  //         type: 'custom',
+  //         renderCustom: function (swiper, current, total) {
+  //           return current + ' of ' + total;
+  //         },
+  //       },
+  //     },
+  //   },
+  // };
+  // const astivateSlider = function () {
+  //   slider = new window.Swiper('.swiper-container', slider);
+  // };
+  // astivateSlider();
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-
-console.log(swiper);
+  const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      type: 'bullets',
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+      },
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      // when window width is >= 1366px
+      1366: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        slidesPerGroup: 4,
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        slidesPerGroup: 2,
+        pagination: {
+          type: 'bullets',
+        },
+      },
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        slidesPerGroup: 2,
+        pagination: {
+          type: 'custom',
+          renderCustom: function (swiper, current, total) {
+            return current + ' of ' + total;
+          },
+        },
+      },
+    },
+  });
+})();
