03d71c0cef07e67b7eeaee9ace45d8a337e9d88d
Blazelip <filin1994@rambler.ru>
2021-04-22 15:34:58 +0300

    Попытка сделать слайдер

--- build/js/main.js
+++ build/js/main.js
@@ -16,56 +16,6 @@
 'use strict';
 
 (() => {
-  // const slider = {
-  //   loop: true,
-  //   loopFillGroupWithBlank: true,
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     clickable: true,
-  //     type: 'bullets',
-  //     renderBullet: function (index, className) {
-  //       return '<span class="' + className + '">' + (index + 1) + '</span>';
-  //     },
-  //   },
-  //   navigation: {
-  //     nextEl: '.swiper-button-next',
-  //     prevEl: '.swiper-button-prev',
-  //   },
-  //   breakpoints: {
-  //     // when window width is >= 1366px
-  //     1366: {
-  //       slidesPerView: 4,
-  //       spaceBetween: 30,
-  //       slidesPerGroup: 4,
-  //     },
-  //     // when window width is >= 768px
-  //     768: {
-  //       slidesPerView: 2,
-  //       spaceBetween: 30,
-  //       slidesPerGroup: 2,
-  //       pagination: {
-  //         type: 'bullets',
-  //       },
-  //     },
-  //     // when window width is >= 320px
-  //     320: {
-  //       slidesPerView: 2,
-  //       spaceBetween: 30,
-  //       slidesPerGroup: 2,
-  //       pagination: {
-  //         type: 'custom',
-  //         renderCustom: function (swiper, current, total) {
-  //           return current + ' of ' + total;
-  //         },
-  //       },
-  //     },
-  //   },
-  // };
-  // const astivateSlider = function () {
-  //   slider = new window.Swiper('.swiper-container', slider);
-  // };
-  // astivateSlider();
-
   const swiper = new Swiper('.swiper-container', {
     loop: true,
     loopFillGroupWithBlank: true,
@@ -82,21 +32,6 @@
       prevEl: '.swiper-button-prev',
     },
     breakpoints: {
-      // when window width is >= 1366px
-      1366: {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        slidesPerGroup: 4,
-      },
-      // when window width is >= 768px
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-        slidesPerGroup: 2,
-        pagination: {
-          type: 'bullets',
-        },
-      },
       // when window width is >= 320px
       320: {
         slidesPerView: 2,
@@ -108,6 +43,24 @@
             return current + ' of ' + total;
           },
         },
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        slidesPerGroup: 2,
+        pagination: {
+          type: 'bullets',
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+          },
+        },
+      },
+      // when window width is >= 1366px
+      1366: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+        slidesPerGroup: 4,
       },
     },
   });
