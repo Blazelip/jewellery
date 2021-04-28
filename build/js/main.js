1ee8952e42f0dcd11317126bcaa9dd88c5c70156
Blazelip <filin1994@rambler.ru>
2021-04-28 15:16:05 +0300

    Перепилывает логику dropdown блоков

--- build/js/main.js
+++ build/js/main.js
@@ -9698,43 +9698,26 @@
 
 (() => {
   document.addEventListener(`DOMContentLoaded`, () => {
-    const faqList = document.querySelector(`.faq__list`);
-    const faqItems = document.querySelectorAll(`.faq__item--nojs`);
 
-    const filterSection = document.querySelector(`.filters`);
-    const filtersForm = document.querySelector(`.filters__form`);
-    const filters = document.querySelectorAll(`.dropdown--nojs`);
+    const dropdowns = document.querySelectorAll(`.dropdown`);
+    const dropdownBtns = document.querySelectorAll(`.dropdown__btn`);
 
-    const onFilterClick = (evt) => {
-      const target = evt.target;
-      if (target.closest(`.dropdown__btn`)) {
-        target.closest(`.dropdown`).classList.toggle(`dropdown--opened`);
+    const onDropdownBtnClick = (btn) => {
+      if (btn) {
+        btn.closest(`.dropdown`).classList.toggle(`dropdown--opened`);
       }
     };
 
-    const onFaqItemClick = (evt) => {
-      const item = evt.target.closest(`.faq__item`);
-      item.classList.toggle(`faq__item--opened`);
-    };
-
-    if (filterSection) {
-      filterSection.classList.remove(`filters--nojs`);
-    }
-
-    if (filtersForm) {
-      filters.forEach((item) => {
+    if (dropdowns) {
+      dropdowns.forEach((item) => {
         item.classList.remove(`dropdown--nojs`);
       });
 
-      filtersForm.addEventListener(`click`, onFilterClick);
-    }
-
-    if (faqList) {
-      faqItems.forEach((item) => {
-        item.classList.remove(`faq__item--nojs`);
+      dropdownBtns.forEach((btn) => {
+        btn.addEventListener(`click`, () => {
+          onDropdownBtnClick(btn);
+        });
       });
-
-      faqList.addEventListener(`click`, onFaqItemClick);
     }
   });
 })();
